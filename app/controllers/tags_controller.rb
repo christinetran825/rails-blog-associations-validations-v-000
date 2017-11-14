11a7da74baad75a21f8bfe7d226438aacd1e04b7
Christine Tran <christinetran825@gmail.com>
2017-11-14 04:08:20 +0000

    automatically backed up by learn

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,32 @@
 class TagsController < ApplicationController
+
+  before_action :set_tag, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tags = Tag.all
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def destroy
+    @tag.delete
+    redirect_to tag_path(@tag)
+  end
+
 end

4e20f1e1542650a1829f4044690eca854198e28a
Christine Tran <christinetran825@gmail.com>
2017-11-14 04:34:50 +0000

    automatically backed up by learn

--- spec/02_features/tags_for_post_spec.rb
+++ spec/02_features/tags_for_post_spec.rb
@@ -23,8 +23,8 @@ describe 'tags for posts', :type => :feature do
 
   it 'can create a post with adding multiple tags' do
     visit '/posts/new'
-    fill_in('Name', :with => 'Post title')
-    fill_in('Content', :with => 'post content')
+    fill_in('name', :with => 'Post title')
+    fill_in('content', :with => 'post content')
     check('cute')
     check('adorable')
     click_button('Create Post')
